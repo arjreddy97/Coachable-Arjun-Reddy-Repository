123cdb34f55185c15ff10522a450e5a4788a5c4d
arjreddy97 <107497684+arjreddy97@users.noreply.github.com>
2024-12-30 17:01:57 -0500

    162. Find Peak Element

---
+++ leetcode/Peak Element.py
@@ -0,0 +1,36 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+                
+        frequencyTable = getFreqTable(nums)
+        frequencyScores = getTuples(frequencyTable)
+        
+        heapq.heapify(frequencyScores)
+        counter = k
+        
+        kFreqElements = []
+        while counter != 0 and len(frequencyScores):
+            
+            currentElement = heapq.heappop(frequencyScores)
+            currentFreq,currentWord = currentElement
+            print(currentWord)
+            kFreqElements.append(currentWord)
+            counter -= 1
+        
+        return kFreqElements
+        
+    
+def getTuples(freqTable):
+    tuples = []
+    for word in freqTable:
+        tuples.append((-freqTable[word],word))
+    return tuples
+    
+        
+def getFreqTable(words):
+    table = {}
+    for word in words:
+        if word not in table:
+            table[word] = 0
+        table[word] += 1
+    return table
