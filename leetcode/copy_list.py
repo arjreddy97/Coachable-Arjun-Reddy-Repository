46768679e86825b6a9d967ac5ad11f3d7157b29f
arjreddy97 <107497684+arjreddy97@users.noreply.github.com>
2025-01-21 14:48:07 -0500

    Update copy_list.py

--- leetcode/copy_list.py
+++ leetcode/copy_list.py
@@ -1,29 +1,49 @@
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        seen = {}
-        newHead = None
-        curr = head
-        while curr != None:
-            newNode = Node(curr.val)
-            seen[curr] = newNode
-            curr = curr.next
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        createClones(head)
+        populateRandomPointers(head)
+        return getClonedList(head)
         
-        curr = head
-        while curr != None:
-            copy_node = seen[curr]
-            if newHead == None:
-                newHead = copy_node
-            original_next = curr.next if curr.next else None
-            original_rp = curr.random if curr.random else None
+def getClonedList(head):
+    if not head:
+        return 
+    current = head
+    newListHead = current.next
+    while current.next is not None:
+        nextNode = current.next
+        current.next = current.next.next
+        current = nextNode
+    return newListHead
+        
+        
+def populateRandomPointers(head):
+    current = head
+    while current is not None:
+        current.next.random = None if current.random is None else current.random.next
+        current = current.next.next
+    
+def createClones(head):
+    current = head
+    while current is not None:
+        nextNode = current.next
+        clone = Node(current.val)
+        current.next = clone
+        clone.next = nextNode
+        current = nextNode
+    
+        
 
-            copy_node_next = None if original_next == None else seen[original_next]
-            copy_node_random = None if original_rp == None else seen[original_rp]
-
-            
-
-            copy_node.next = copy_node_next
-            copy_node.random = copy_node_random
-
-            curr = curr.next
-
-        return newHead
+    
