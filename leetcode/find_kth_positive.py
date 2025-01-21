7874c91a9a4d9d45fb13360556b0169a77c7efbb
arjreddy97 <107497684+arjreddy97@users.noreply.github.com>
2025-01-21 14:49:03 -0500

    Delete leetcode/find_kth_positive.py

--- leetcode/find_kth_positive.py
+++
@@ -1,9 +0,0 @@
-
- class Solution:
-	def findKthPositive(self, arr: List[int], k: int) -> int:
-		arr = set(arr)
-		for i in range(1, len(arr)+k+1):  # O(n+k)
-			if i not in arr:
-				k -= 1
-			if k == 0:
-				return i
